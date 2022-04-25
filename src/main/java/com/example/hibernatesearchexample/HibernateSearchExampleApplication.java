c82c3c38e3f806b032a0ccdfd4ebb4115164376c
Pawel Polewczak <pawel.polewczak@codelab.eu>
2022-04-25 19:49:39 +0200

    Initial commit

---
+++ src/main/java/com/example/hibernatesearchexample/HibernateSearchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernatesearchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSearchExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateSearchExampleApplication.class, args);
+    }
+
+}
